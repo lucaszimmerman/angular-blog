f458b1b072a9e02139a690a0d650f722a5c62787
lucaszimmerman <lucaszimmermants@gmail.com>
2023-09-22 20:45:21 -0300

    add dinamic atributes

--- src/app/components/title-blog/title-blog.component.ts
+++ src/app/components/title-blog/title-blog.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title-blog',
   templateUrl: './title-blog.component.html',
-  styleUrls: ['./title-blog.component.css']
+  styleUrls: ['./title-blog.component.css', './title-blog.responsive.component.css']
 })
 export class TitleBlogComponent {
 
