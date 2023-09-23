db7b44d5ffae77818bb9218904619372f2f77320
lucaszimmerman <lucaszimmermants@gmail.com>
2023-09-22 22:42:40 -0300

    page content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+  "id":"1",
+  "photoCover":"https://ichef.bbci.co.uk/news/2048/cpsprodpb/858e/live/f6a50940-5782-11ee-b44c-45cc4947521f.jpg",
+  "title": "Foram revelados os 50 maiores ratings do EAFC24",
+  "description": "Confira os 50 maiores ratings do novo jogo da EA Sports."
+},
+{
+  "id":"2",
+  "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+  "title": "Confira tudo sobre as séries da marvel e sua importância para o MCU.",
+  "description": "ola"
+}
+]
